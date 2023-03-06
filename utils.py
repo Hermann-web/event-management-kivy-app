029b8476cdf73e67ba97a43ab29024d88c6d030d
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2023-03-06 02:02:18 +0100

    crete decorator to handle exceptions

---
+++ utils.py
@@ -0,0 +1,29 @@
+import traceback
+from config import logging 
+
+
+def parse_mongo_obj_to_json_serializable(list_obj):
+    list_obj = list(list_obj)
+    for c in list_obj:
+        c['_id'] = str(c['_id'])
+    return list_obj
+
+def log_exception(err, action):
+    detail_ = f"--> action: {action}\n err: {err}"
+    error_message = "EXCEPTION:\n" + detail_
+    exception_details = traceback.format_exc()
+    logging.error(f"An exception occurred: {error_message}")
+    logging.error(f"Exception details:\n{exception_details}")
+
+def catch_exceptions(func):
+    def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        logging.debug(f"function call: {func_name}")
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            logging.info(f"function details: args={args} kwargs={kwargs}")
+            log_exception(err=e, action=f"wrapping {func_name} exceptions (RETURNING NONE): \nmodule:{func.__module__} \nannotations:{func.__annotations__} \ndocstring:{func.__doc__}")
+            result = None
+        return result
+    return wrapper
