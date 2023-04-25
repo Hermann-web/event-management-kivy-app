df7298766547cb456746648b8415af520c5bc6f0
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2023-04-25 01:58:07 +0100

    change place of get_database

---
+++ config/db.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__      = "Hermann Agossou"
+
+from pymongo import MongoClient, errors #do not remove (errors is used elsewhere)
+import sqlite3
+from config.config import PROD_ENV, ONLINE
+from config.config import CONNECTION_STRING, DATABASE_NAME
+from config.config import SQLITE_DB
+from config.config import logging 
+
+logging.info(f"...PROD_ENV: is set to {PROD_ENV}")
+logging.info(f"...DATABASE_NAME: {DATABASE_NAME}")
+logging.info(f"...ONLINE: is set to {ONLINE}")
+
+
+
+def get_database():
+    logging.info(f"database call: {DATABASE_NAME}")
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING)
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client, client[DATABASE_NAME]
--- setup.py
+++ setup.py
@@ -2,46 +2,31 @@
 # -*- coding: utf-8 -*-
 __author__      = "Hermann Agossou"
 
-from pymongo import MongoClient, errors #do not remove (errors is used elsewhere)
-from config.config import PROD_ENV, ONLINE
-from config.config import CONNECTION_STRING, DATABASE_NAME
-from config.config import logging 
-
-logging.info(f"...PROD_ENV: is set to {PROD_ENV}")
-logging.info(f"...DATABASE_NAME: {DATABASE_NAME}")
-logging.info(f"...ONLINE: is set to {ONLINE}")
-
-
-
-def get_database():
-    logging.info(f"database call: {DATABASE_NAME}")
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
-    # Create the database for our example (we will use the same database throughout the tutorial
-    return client, client[DATABASE_NAME]
-
+from config.config import PROD_ENV, logging
 
 def setup():
     from backend.preprocess_json_for_colllection import clean_clients, clean_events, clean_user_events
     from backend.mongo_backup_and_load import import_json_data_to_mongodb_atlas
     
 
-    print(f"....clean_clients starting")
+    logging.info("setup: ....clean_clients starting")
     clean_clients()
-    print(f"....clean_clients ended")
+    logging.info("setup: ....clean_clients ended")
 
-    print(f"....clean_events starting")
+    logging.info("setup: ....clean_events starting")
     clean_events()
-    print(f"....clean_events ended")
+    logging.info("setup: ....clean_events ended")
 
-    print(f"....clean_user_events starting")
+    logging.info("setup: ....clean_user_events starting")
     clean_user_events()
-    print(f"....clean_user_events ended")
+    logging.info("setup: ....clean_user_events ended")
 
-    print(f"....send_data_online starting")
-    if not PROD_ENV: import_json_data_to_mongodb_atlas()
-    else: print("... not doing this !!!")
-    print(f"....send_data_online ended")
+    logging.info("setup: ....send_data_online starting")
+    if not PROD_ENV: 
+        import_json_data_to_mongodb_atlas()
+        logging.info("setup: ....send_data_online ended")
+    else: logging.info("setup: ... not doing this !!!")
+    
 
 if __name__ =="__main__":
     setup()
